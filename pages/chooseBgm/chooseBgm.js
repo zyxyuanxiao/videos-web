d346bf97c50ff63f47ce7d3673f3b8d2491c452c
songyouyu <songyy950208@163.com>
2019-05-12 21:06:22 +0800

    视频上传

---
+++ pages/chooseBgm/chooseBgm.js
@@ -0,0 +1,144 @@
+const app = getApp()
+
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        bgmList: [],
+        serverUrl: "",
+        videoParams: {}
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(params) {
+        var me = this;
+        var serverUrl = app.serverUrl;
+        me.setData({
+            videoParams: params
+        })
+        wx.showLoading({
+            title: '请等待...',
+        });
+        wx.request({
+            url: serverUrl + '/bgm/list',
+            method: 'POST',
+            header: {
+                'content-type': 'application/json'
+            },
+            success: function(res) {
+                console.log(res)
+                wx.hideLoading();
+                if (res.data.status == 200) {
+                    var bgmList = res.data.data;
+                    me.setData({
+                        bgmList: bgmList,
+                        serverUrl: serverUrl
+                    });
+                } else if (res.data.status == 502) {
+                    wx.showToast({
+                        title: res.data.msg,
+                        duration: 2000,
+                        icon: "none",
+                        success: function() {
+                            wx.redirectTo({
+                                url: '../userLogin/login',
+                            })
+                        }
+                    });
+                }
+            }
+        })
+    },
+
+    upload: function(e) {
+        var me = this;
+        console.log(e)
+        var bgmId = e.detail.value.bgmId;
+        var desc = e.detail.value.desc;
+
+        var duration = me.data.videoParams.duration;
+        var tmpHeight = me.data.videoParams.tmpHeight;
+        var tmpWidth = me.data.videoParams.tmpWidth;
+        var tmpVideoUrl = me.data.videoParams.tmpVideoUrl;
+        var tmpCoverUrl = me.data.videoParams.tmpCoverUrl;
+        
+        wx.showLoading({
+            title: '上传中...',
+        });
+
+        var serverUrl = app.serverUrl;
+        var userInfo = app.userInfo;
+
+        wx.uploadFile({
+            url: serverUrl + '/video/upload',
+            formData: {
+                userId: userInfo.id,
+                bgmId: bgmId,
+                desc: desc,
+                videoSeconds: duration,
+                videoHeight: tmpHeight,
+                videoWidth: tmpWidth
+            },
+            filePath: tmpVideoUrl,
+            name: 'file',
+            header: {
+                'content-type': 'application/json', // 默认值
+            },
+            success: function(res) {
+				wx.hideLoading();
+                var data = JSON.parse(res.data);
+                if (data.status == 200) {
+					wx.showToast({
+						title: '上传成功!',
+						icon: "success"
+					});
+
+                    // var videoId = data.data;
+
+                    // wx.uploadFile({
+                    //     url: serverUrl + '/video/uploadCover',
+                    //     formData: {
+                    //         userId: app.userInfo.id,
+                    //         videoId: videoId
+                    //     },
+                    //     filePath: tmpCoverUrl,
+                    //     name: 'file',
+                    //     header: {
+                    //         'content-type': 'application/json' // 默认值
+                    //     },
+                    //     success: function(res) {
+                    //         var data = JSON.parse(res.data);
+                    //         wx.hideLoading();
+                    //         if (data.status == 200) {
+                    //             wx.showToast({
+                    //                 title: '上传成功!~~',
+                    //                 icon: "success"
+                    //             }); 
+                    //         } else {
+                    //             wx.showToast({
+                    //                 title: '上传失败!~~',
+                    //                 icon: "success"
+                    //             });
+                    //         }
+                    //     }
+                    // })
+                } else if (res.data.status == 502) {
+                    wx.showToast({
+                        title: res.data.msg,
+                        duration: 2000,
+                        icon: "none"
+                    });
+                } else {
+                    wx.showToast({
+                        title: '上传失败!~~',
+                        icon: "success"
+                    });
+                }
+            }
+        })
+    }
+})
