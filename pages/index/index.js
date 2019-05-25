ddb3909a21bd212c8518a7bcef48846903dab87c
songyouyu <songyy950208@163.com>
2019-05-25 16:40:07 +0800

    视频展示页

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,87 @@
-//index.js
-//获取应用实例
 const app = getApp()
 
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
+    data: {
+		// 用于分页的属性
+		totalPage: 1,
+		pageNum: 1,
+		screenWidth: 350,
+		videoList: [],
+		serverUrl: ''
+    },
+
+    onLoad: function(params) {
+		var me = this;
+		
+		var screenWidth = wx.getSystemInfoSync().windowWidth;
+		me.setData({
+			screenWidth: screenWidth
+		});
+
+		// 获取当前的分页数
+		var pageNum = me.data.pageNum;
+		me.getAllVideoList(pageNum);
+    },
+
+	getAllVideoList: function (pageNum) {
+		var me = this;
+		wx.showLoading({
+			title: '加载中，请等待...',
+		})
+
+		var serverUrl = app.serverUrl;
+		wx.request({
+			url: serverUrl + '/video/queryAll?pageNum=' + pageNum,
+			method: 'POST',
+			success: function (res) {
+				console.log(res.data)
+				wx.hideLoading();
+				wx.hideNavigationBarLoading();
+				// 停止当前页面下拉刷新
+				wx.stopPullDownRefresh();
+
+				// 判断当前页pageNum是否是第一页，如果是第一页，那么设置videoList为空
+				if (pageNum === 1) {
+					me.setData({
+						videoList: []
+					});
+				}
+
+				var videoList = res.data.data.rows;
+				var newVideoList = me.data.videoList;
+
+				me.setData({
+					videoList: newVideoList.concat(videoList),
+					pageNum: pageNum,
+					totalPage: res.data.data.total,
+					serverUrl: serverUrl
+				});
+			}
+		})
+	},
+
+	// 上拉刷新
+	onReachBottom: function () {
+		var me = this;
+		var currentPage = me.data.pageNum;
+		var totalPage = me.data.totalPage;
+
+		// 判断当前页数和总页数是否相等，如果想的则无需查询
+		if (currentPage === totalPage) {
+			wx.showToast({
+				title: '已经没有视频啦~~',
+				icon: "none"
+			})
+			return;
+		}
+
+		var pageNum = currentPage + 1;
+		me.getAllVideoList(pageNum);
+	},
+
+	// 下拉刷新，需要在 index.json 配置
+	onPullDownRefresh: function () {
+		wx.showNavigationBarLoading();
+		this.getAllVideoList(1);
+	},
 })
